c9377ff812c881ba06d66019d91908fd4c2f5994
SimoCrash <simone.crescenzi@hotmail.com>
2023-04-13 17:02:48 +0200

    Fine Esercizio

---
+++ la-mia-pizzeria-static/Attributes/ParoleMultipleAttribute.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace la_mia_pizzeria_static.Attributes
+{
+    public class ParoleMultipleAttribute : ValidationAttribute
+    {
+        int contaParole;
+        public ParoleMultipleAttribute(int contaParole)
+        {
+            this.contaParole = contaParole;
+        }
+
+        protected override ValidationResult IsValid(object? value, ValidationContext _)
+        {
+            var input = value as string;
+
+            if (input is null || input.Trim().Split(' ').Length < contaParole)
+            {
+                return new ValidationResult($"Inserire almeno {contaParole} parole");
+            }
+
+            return ValidationResult.Success!;
+        }
+    }
+}
+
--- la-mia-pizzeria-static/Models/Pizza.cs
+++ la-mia-pizzeria-static/Models/Pizza.cs
@@ -1,4 +1,5 @@
 ﻿
+using la_mia_pizzeria_static.Attributes;
 using System.ComponentModel.DataAnnotations;
 
 namespace la_mia_pizzeria_static.Models
@@ -9,14 +10,17 @@ namespace la_mia_pizzeria_static.Models
 
         [Required(ErrorMessage = "Inserisci il Nome della pizza che vuoi aggiungere.")]
         [StringLength(100, ErrorMessage = "Puoi inserire fino a un massimo di 100 caratteri")]
+        [ParoleMultiple(5)]
         public string Nome { get; set; } = string.Empty;
 
         [Required(ErrorMessage = "Inserisci la Descrizione della pizza che vuoi aggiungere.")]
         public string Descrizione { get; set; } = string.Empty;
 
-        public string? Foto { get; set; } = string.Empty;
+        [Required(ErrorMessage = "Inserisci una foto della pizza che vuoi aggiungere.")]
+        public string? Foto { get; set; }
 
         [Required(ErrorMessage = "Inserisci il Prezzo della pizza che vuoi aggiungere.")]
+        [Range(1, 30, ErrorMessage = "Inserisci il Prezzo della pizza che vuoi aggiungere, superiore a 1.")]
         public string Prezzo { get; set; } = string.Empty;
     }
 }
